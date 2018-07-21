9b9fb76b21ac10db7a73c9a22175a27ec4eceecb
Conner Tenn <connertenn@gmail.com>
2018-07-21 00:53:56 -0400

    Added dynamic elements example

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -1,16 +1,44 @@
 
-var accItem = document.getElementsByClassName('accordionItem');
-var accHD = document.getElementsByClassName('accordionItemHeading');
-for (i = 0; i < accHD.length; i++) {
-	accHD[i].addEventListener('click', toggleItem, false);
+var addList = document.getElementsByClassName("add");
+var remList = document.getElementsByClassName("remove");
+var container = document.getElementsByClassName("elementContainer")[0];
+
+for (i = 0; i < addList.length; i++) 
+{
+	addList[i].addEventListener('click', Add);
 }
-function toggleItem() {
-	console.log('Click Event');
-	var itemClass = this.parentNode.className;
+for (i = 0; i < remList.length; i++) 
+{
+	remList[i].addEventListener('click', Remove);
+}
+
+var elementHtml = "<div class=\"element\">< h2 class=\"remove\" > Delete</h2 ></div >";
+
+function Add() 
+{
+	console.log("addList");
+	/*var itemClass = this.parentNode.className;
 	for (i = 0; i < accItem.length; i++) {
 		accItem[i].className = 'accordionItem close';
 	}
 	if (itemClass == 'accordionItem close') {
 		this.parentNode.className = 'accordionItem open';
-	}
+	}*/
+	var newElem = document.createElement("div");
+	newElem.className = "element";
+	var newElemChild = document.createElement("h2");
+	newElemChild.className = "remove";
+	newElemChild.innerHTML="Delete";
+	newElem.appendChild(newElemChild);
+	
+	container.appendChild(newElem);
+	
+	remList = document.getElementsByClassName("remove");
+	remList[remList.length-1].addEventListener('click', Remove);
+}
+
+function Remove()
+{
+	console.log("remove");
+	container.removeChild(this.parentNode);
 }
