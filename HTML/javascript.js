825c9dfbe1044189cd3d2f53c080fe326942ea1d
Conner Tenn <connertenn@gmail.com>
2018-07-24 01:16:51 -0400

    Removed Commented Code

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -4,16 +4,11 @@ var Container = $(".elementContainer")[0];
 //var Template = document.getElementsByClassName("elementTemplate")[0];
 var Template = $(".elementTemplate")[0];
 
-//document.getElementsByClassName("add")[0].addEventListener('click', AddElement);
 $(".add").click(AddElement);
-//document.getElementsByClassName("draw")[0].addEventListener('click', Draw);
 $(".draw").click(Draw);
 
 InitTable();
 AddElement();
-//Container.childNodes[0].getElementsByClassName("course")[0].value = "CIS 9000";
-//Container.childNodes[0].getElementsByClassName("start")[0].value = "8";
-//Container.childNodes[0].getElementsByClassName("end")[0].value = "10";
 $(".elementContainer").find(".course").val("CIS 9000");
 $(".elementContainer").find(".start").val("8");
 $(".elementContainer").find(".end").val("10");
@@ -21,15 +16,9 @@ Draw();
 
 function InitTable() 
 {
-	//var outputTable = document.getElementsByClassName("outputTable")[0];
-	//var outputTable = $(".outputTable")[0];
-	//var times = document.getElementsByClassName("times")[0];
 
 	for (var i = 1; i <= 12; i++) 
 	{
-		//var newElem = document.createElement("tr");
-		//newElem.innerHTML = "<td class=\"time\" num=\"" + i + "\">" + i + "</td><td class=\"chart\" num=\"" + i + "\"></td>";
-		//outputTable.appendChild(newElem);
 		$(".outputTable").append("<tr><td class=\"time\" num=\"" + i + "\">" + i + "</td><td class=\"chart\" num=\"" + i + "\"></td></tr>");
 	}
 }
@@ -40,13 +29,14 @@ function AddElement()
 	newElem.className = "element";
 	Container.appendChild(newElem);
 	
-	//newElem.getElementsByClassName("remove")[0].addEventListener('click', RemoveElement);
-	$(".remove").click(RemoveElement);
+	//$(".remove").click(RemoveElement);
+	$(newElem).children(".remove").click(RemoveElement);
 }
 
 function RemoveElement()
 {
-	Container.removeChild(this.parentNode);
+	//Container.removeChild(this.parentNode);
+	$(this).parent().remove();
 }
 
 
@@ -65,13 +55,8 @@ function NewTableElement(course, start, end)
 function Draw()
 {
 	console.log("draw");
-	//var tableElements = document.getElementsByClassName("TableElements")[0];
-	var tableElements = $(".TableElements")
+	var tableElements = $(".TableElements");
 	
-	/*while (tableElements.hasChildNodes()) 
-	{
-		tableElements.removeChild(tableElements.childNodes[0]);
-	}*/
 	tableElements.empty();
 	
 	//var childNodes = Container.childNodes;
@@ -87,7 +72,6 @@ function Draw()
 		var end = childNodes.children(".end")[i].value;
 		console.log(i + " " + course + " " + start + "-" + end);
 		
-		//tableElements.appendChild(NewTableElement(course, start, end));
 		tableElements.append(NewTableElement(course, start, end));
 		
 	}
