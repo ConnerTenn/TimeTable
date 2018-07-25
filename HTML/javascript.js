ebc05f7b27d00b5afc724abf3bf62d4024f2ea07
Conner Tenn <connertenn@gmail.com>
2018-07-25 05:31:59 -0400

    Switched to using CSS grid

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -1,6 +1,6 @@
 
-//var Container = document.getElementsByClassName("elementContainer")[0];
-var Container = $(".elementContainer")[0];
+//var ListItems = document.getElementsByClassName("listItems")[0];
+var ListItems = $(".listItems")[0];
 //var Template = document.getElementsByClassName("elementTemplate")[0];
 var Template = $(".elementTemplate")[0];
 
@@ -9,17 +9,17 @@ $(".draw").click(Draw);
 
 InitTable();
 AddElement();
-$(".elementContainer").find(".course").val("CIS 9000");
-$(".elementContainer").find(".start").val("8");
-$(".elementContainer").find(".end").val("10");
+$(".listItems").find(".course").val("CIS 9000");
+$(".listItems").find(".start").val("8");
+$(".listItems").find(".end").val("10");
 Draw();
 
 function InitTable() 
 {
 
-	for (var i = 1; i <= 12; i++) 
+	for (var i = 1; i <= 46; i++) 
 	{
-		$(".outputTable").append("<tr><td class=\"time\" num=\"" + i + "\">" + i + "</td><td class=\"chart\" num=\"" + i + "\"></td></tr>");
+		$(".outputTable").append("<div class=\"time\">" + Math.ceil(i/2) + ":" + (i%2 ? "00" : "30") + "</div>");
 	}
 }
 
@@ -27,7 +27,7 @@ function AddElement()
 {
 	var newElem = Template.cloneNode(true);
 	newElem.className = "element";
-	Container.appendChild(newElem);
+	ListItems.appendChild(newElem);
 	
 	//$(".remove").click(RemoveElement);
 	$(newElem).children(".remove").click(RemoveElement);
@@ -35,32 +35,30 @@ function AddElement()
 
 function RemoveElement()
 {
-	//Container.removeChild(this.parentNode);
+	//ListItems.removeChild(this.parentNode);
 	$(this).parent().remove();
 }
 
 
 function NewTableElement(course, start, end)
 {
-	var newElem = document.createElement("div");
-	newElem.className = "TableElement";
-	newElem.innerHTML = course;
-	newElem.style.top = 21 + 30*start + "px";
-	newElem.style.left = 78 + "px";
-	newElem.style.width = "296px";
-	newElem.style.height = 19 + 30*(end-start-1) + "px";
-	return newElem;
+	//var newElem = document.createElement("div");
+	//newElem.className = "course";
+	
+	
+	return "<div class=\"course\" style=\"grid-row: " + (start+1) + "/" + (end+1) + "; grid-column:2/2;\">" + course + "</div>";
 }
 
 function Draw()
 {
 	console.log("draw");
-	var tableElements = $(".TableElements");
+	//var tableElements = $(".TableElements");
+	var outputTable = $(".outputTable");
+	//tableElements.empty();
+	outputTable.children(".course").remove();
 	
-	tableElements.empty();
-	
-	//var childNodes = Container.childNodes;
-	var childNodes = $(".elementContainer").children();
+	//var childNodes = ListItems.childNodes;
+	var childNodes = $(".listItems").children();
 	for (var i = 0; i < childNodes.length; i++)
 	{
 		//var course = childNodes[i].getElementsByClassName("course")[0].value;
@@ -68,11 +66,11 @@ function Draw()
 		//var end = childNodes[i].getElementsByClassName("end")[0].value;
 		
 		var course = childNodes.children(".course")[i].value;
-		var start = childNodes.children(".start")[i].value;
-		var end = childNodes.children(".end")[i].value;
+		var start = parseInt(childNodes.children(".start")[i].value);
+		var end = parseInt(childNodes.children(".end")[i].value);
 		console.log(i + " " + course + " " + start + "-" + end);
 		
-		tableElements.append(NewTableElement(course, start, end));
+		outputTable.append(NewTableElement(course, start, end));
 		
 	}
 }
